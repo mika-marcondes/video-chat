0d5e9c22cc8e5ef059b543b18048b2812de98cab
Mika <mika.marcondes2@gmail.com>
2022-10-14 21:51:55 -0300

    :construction: Create rooms

--- server.js
+++ server.js
@@ -4,4 +4,17 @@ const server = require("http").createServer(app);
 const { Socket } = require("socket.io");
 const io = Socket(server);
 
+const rooms = {};
+
+io.on("Connection", (Socket) => {
+  Socket.on("Join room", (roomID) => {
+    if (rooms[roomID]) {
+      rooms[roomID].push(Socket.id);
+    } else {
+      rooms[roomID] = [Socket.id];
+    }
+    const otherUser = rooms[roomID].find((id) => id !== Socket.id);
+  });
+});
+
 server.listen(8000, () => console.log("Server is running on port 8000"));
